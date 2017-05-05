a4756249c297bfc7692db1eee0a18c4ea7bd57fc
Shiburagi <tr32010@gmail.com>
2017-05-05 18:21:53 +0800

    Meter View added

--- stylishwidget/src/main/java/com/app/infideap/stylishwidget/view/AMeter.java
+++ stylishwidget/src/main/java/com/app/infideap/stylishwidget/view/AMeter.java
@@ -13,7 +13,6 @@ import android.os.Build;
 import android.support.annotation.Px;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
